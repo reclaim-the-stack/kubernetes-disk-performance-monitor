1cca1561aabb072d43974679eea585f2f8945d94
David Backeus <duztdruid@gmail.com>
2025-12-01 10:24:27 +0100

    Configurable intervals, use megabytes, add deployment example

--- README.md
+++ README.md
@@ -2,7 +2,23 @@
 
 Writes a file to disk, then reads it, on a set interval and exposes metrics on a `/metrics` endpoint.
 
-## Example output
+## Local run
+
+Prerequisites: Ruby 3.4.7
+
+Install dependencies: `bundle install`.
+
+Start the app: `bundle exec puma`.
+
+## Configuration
+
+The app is configured via ENV variables.
+
+`MEGABYTES_PER_BLOCK` - How many megabytes to write per block (default: 1)
+`BLOCKS_PER_ITERATION` - How many blocks to write per iteration (default: 100)
+`INTERVAL_IN_SECONDS` - How many seconds to wait per benchmark run (default: 60)
+
+## Example metrics output
 
 ```
 # HELP disk_read_bytes_per_second Disk read bytes per second
@@ -12,3 +28,9 @@ disk_read_bytes_per_second %<read_bytes_per_second>
 # TYPE disk_write_bytes_per_second gauge
 disk_write_bytes_per_second %<write_bytes_per_second>
 ```
+
+## Kubernetes Deployment
+
+Prerequisites: `prometheus-operator` must be present in the target cluster for the `ServiceMonitor` to work.
+
+See example files under `/deploy` and adjust to your environment.
--- main.rb
+++ main.rb
@@ -1,20 +1,26 @@
 # frozen_string_literal: true
 
+require "bundler/setup"
+
 require "active_support/all"
 require "fileutils"
 require "rack"
 require "securerandom"
 
+ENV["MEGABYTES_PER_BLOCK"] ||= "1"
+ENV["BLOCKS_PER_ITERATION"] ||= "100"
+ENV["INTERVAL_IN_SECONDS"] ||= "60"
+
 module DiskBench
-  BLOCK_SIZE = 1.megabyte
+  BLOCK_SIZE = ENV.fetch("MEGABYTES_PER_BLOCK").to_i.megabyte
   BLOCK = "\0" * BLOCK_SIZE
 
-  def self.run(blocks: 10)
-    path = "diskbench-#{SecureRandom.hex(8)}.bin"
+  def self.run(blocks: ENV.fetch("BLOCKS_PER_ITERATION").to_i)
+    path = "diskbench-#{SecureRandom.hex(8)}.bin" # use unique files to avoid potential caching effects
     write_result = write_test(path:, blocks:)
     read_result = read_test(path:)
 
-    { write: write_result, read: read_result }
+    { write: write_result, read: read_result, time_taken: write_result[:elapsed] + read_result[:elapsed] }
   ensure
     FileUtils.rm_f(path) if path
   end
@@ -35,10 +41,8 @@ module DiskBench
     elapsed = Process.clock_gettime(Process::CLOCK_MONOTONIC) - start
 
     {
-      op: "write",
-      path:,
-      bytes: bytes_written,
-      seconds: elapsed,
+      bytes_written:,
+      elapsed:,
       bytes_per_second: bytes_written / elapsed,
       block_size: BLOCK_SIZE,
     }
@@ -57,10 +61,8 @@ module DiskBench
     elapsed = Process.clock_gettime(Process::CLOCK_MONOTONIC) - start
 
     {
-      op: "read",
-      path:,
       bytes: bytes_read,
-      seconds: elapsed,
+      elapsed: elapsed,
       bytes_per_second: bytes_read.to_f / elapsed,
       block_size: BLOCK_SIZE,
     }
@@ -71,12 +73,12 @@ end
 class MetricsEndpoint
   HEADERS = { "Content-Type" => "text/plain" }.freeze
   RESPONSE_TEMPLATE = <<~METRICS
-    # HELP disk_read_bytes_per_second Disk read bytes per second
-    # TYPE disk_read_bytes_per_second gauge
-    disk_read_bytes_per_second %<read_bytes_per_second>s
-    # HELP disk_write_bytes_per_second Disk write bytes per second
-    # TYPE disk_write_bytes_per_second gauge
-    disk_write_bytes_per_second %<write_bytes_per_second>s
+    # HELP disk_read_megabytes_per_second Disk read megabytes per second
+    # TYPE disk_read_megabytes_per_second gauge
+    disk_read_megabytes_per_second %<read_megabytes_per_second>s
+    # HELP disk_write_megabytes_per_second Disk write megabytes per second
+    # TYPE disk_write_megabytes_per_second gauge
+    disk_write_megabytes_per_second %<write_megabytes_per_second>s
   METRICS
 
   def call(env)
@@ -85,8 +87,8 @@ class MetricsEndpoint
 
       response = format(
         RESPONSE_TEMPLATE,
-        read_bytes_per_second: $disk_read_bytes_per_second,
-        write_bytes_per_second: $disk_write_bytes_per_second,
+        read_megabytes_per_second: $disk_read_megabytes_per_second,
+        write_megabytes_per_second: $disk_write_megabytes_per_second,
       )
 
       [200, HEADERS, [response]]
@@ -98,21 +100,18 @@ class MetricsEndpoint
   private
 
   def ready?
-    $disk_read_bytes_per_second && $disk_write_bytes_per_second
+    $disk_read_megabytes_per_second && $disk_write_megabytes_per_second
   end
 end
 
 Thread.new do
-  loop do
-    result = DiskBench.run(blocks: 100)
-    $disk_read_bytes_per_second = result[:read][:bytes_per_second]
-    $disk_write_bytes_per_second = result[:write][:bytes_per_second]
+  (1..).each do |i|
+    result = DiskBench.run
+    $disk_read_megabytes_per_second = (result[:read][:bytes_per_second] / 1.megabyte).round
+    $disk_write_megabytes_per_second = (result[:write][:bytes_per_second] / 1.megabyte).round
 
-    read_megabytes = $disk_read_bytes_per_second / 1.megabyte
-    write_megabytes = $disk_write_bytes_per_second / 1.megabyte
+    puts "iteration=#{i} time_taken=#{result[:time_taken]} read_megabytes_per_second=#{$disk_read_megabytes_per_second} write_megabytes_per_second=#{$disk_write_megabytes_per_second}" # rubocop:disable Layout/LineLength
 
-    puts "Disk Bench Results: Read #{read_megabytes.to_i} MB/s, Write #{write_megabytes.to_i} MB/s"
-
-    sleep 1.minute
+    sleep ENV.fetch("INTERVAL_IN_SECONDS").to_i
   end
 end
